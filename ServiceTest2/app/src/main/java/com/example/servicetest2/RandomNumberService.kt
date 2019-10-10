2d8cea8742f89b80626aecffede59249198cef8a
Tiago Marinho <tiago.marinho.797@gmail.com>
2019-10-09 23:57:41 -0300

    up ServiceTest

---
+++ ServiceTest2/app/src/main/java/com/example/servicetest2/RandomNumberService.kt
@@ -0,0 +1,46 @@
+package com.example.servicetest2
+
+import android.app.Service
+import android.content.Intent
+import android.os.Handler
+import android.os.IBinder
+import org.joda.time.DateTime
+import java.util.*
+
+
+class RandomNumberService : Service() {
+
+    private lateinit var mHandler: Handler
+    private lateinit var mRunnable: Runnable
+
+    override fun onBind(intent: Intent): IBinder? {
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        // Send a notification that service is started
+        toast("Service started.")
+
+        // Do a periodic task
+        mHandler = Handler()
+        mRunnable = Runnable { showRandomNumber() }
+        mHandler.postDelayed(mRunnable, 5000)
+
+        return Service.START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        toast("Service destroyed.")
+        mHandler.removeCallbacks(mRunnable)
+    }
+
+    // Custom method to do a task
+    private fun showRandomNumber() {
+        val rand = Random()
+        val number = rand.nextInt(100)
+        toast("Random Number : $number")
+//        toast(DateTime.now().toString("dd-MM-yyyy"))
+        mHandler.postDelayed(mRunnable, 5000)
+    }
+}
